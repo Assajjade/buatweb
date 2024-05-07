497c3d135239d9e5b98313887734b97f3373af84
assajjade <abdillah.assajjad@ui.ac.id>
2024-05-07 14:05:36 +0700

    Testing for first push

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main),
     path('panduan/', show_Panduan),
-    path('lokal/', show_Lokal),
+    path('lokal/', form),
     path('pr/', show_Pr),
     path('primary/', show_Primary),
     path('sec/', show_Sec),
--- main/views.py
+++ main/views.py
@@ -47,5 +47,48 @@ def show_Sec(request):
 
     return render(request, "Sec.html")
 
+def form(request):
+    x = np.array([22.035, 32.33, 40.585, 53.635, 62.1])
+    y = np.array([0.101787, 0.046856, 0.03666, 0.0176, 0.01217])
+    degree = 2
+    p = np.polyfit(x, np.log(y), degree)
+    p[-1] = 0
+    b = 0.01;
+
+    if request.method == 'POST':
+        b = float(request.POST.get('masukan'))
+        if b <= 0.00680943:
+            context = {
+            'Nilai_x': "ERROR",
+            }
+            return render(request, "Lokal.html", context)
+        x_extended = np.linspace(0, 120, 200)
+        y_extended_fit = np.exp(np.polyval(p, x_extended))
+
+        index = np.argmin(np.abs(y_extended_fit - b))
+        x_value = x_extended[index]
+
+        print("Nilai x_extended yang sesuai adalah:", b)
+        # Create your views here.
+        context = {
+            'Nilai_x': x_value,
+        }
+        return render(request, "Lokal.html", context)
+
+    x_extended = np.linspace(0, 120, 200)
+    y_extended_fit = np.exp(np.polyval(p, x_extended))
+
+    index = np.argmin(np.abs(y_extended_fit - b))
+    x_value = x_extended[index]
+
+    print("Nilai x_extended yang sesuai adalah:", x_value)
+    # Create your views here.
+    context = {
+        'Nilai_x': x_value,
+    }
+    
+        
+    return render(request, "Lokal.html", context)
+
 
 
